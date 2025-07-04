6cece9188c7e9dd539169a882f52faeae3b43778
kurokood <villarinmon@gmail.com>
2025-07-04 22:04:28 +0800

    updated main config

--- Iac/main.tf
+++ Iac/main.tf
@@ -1,16 +1,9 @@
 terraform {
-  required_version = ">= 1.10.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
-  }
-  backend "s3" {
-    bucket         = "terraform-state-bucket-abc1"
-    key            = "recipe-sharing-app/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-up-and-running-locks"
   }
 }
 
@@ -18,163 +11,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_vpc" "default" {
-  default = true
+data "aws_ssm_parameter" "recipe_latest_ami_id" {
+  name = "/aws/service/canonical/ubuntu/server/22.04/stable/current/amd64/hvm/ebs-gp2/ami-id"
 }
 
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
+data "aws_availability_zones" "available" {
+  state = "available"
 }
 
-resource "aws_security_group" "recipe_sharing_app_alb" {
-  name        = "recipe_sharing_app_alb"
-  description = "Allow all inbound traffic for ALB"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "recipe_sharing_app_asg" {
-  name        = "recipe_sharing_app_asg"
-  description = "Allow inbound traffic from ALB"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = [aws_security_group.recipe_sharing_app_alb.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "recipe_sharing_app" {
-  name               = "recipe-sharing-app"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.recipe_sharing_app_alb.id]
-  subnets            = data.aws_subnets.default.ids
-  ip_address_type    = "ipv4"
-}
-
-resource "aws_lb_target_group" "recipe_sharing_app" {
-  name     = "recipe-sharing-app"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = data.aws_vpc.default.id
-}
-
-resource "aws_lb_listener" "recipe_sharing_app" {
-  load_balancer_arn = aws_lb.recipe_sharing_app.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.recipe_sharing_app.arn
-  }
-}
-
-resource "aws_iam_role" "recipe_sharing_app_ec2_role" {
-  name = "recipe_sharing_app_ec2_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "dynamodb_access" {
-  name        = "dynamodb_access_policy"
-  description = "Policy for DynamoDB access"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "dynamodb:PutItem",
-          "dynamodb:Scan",
-          "dynamodb:DeleteItem"
-        ]
-        Effect   = "Allow"
-        Resource = aws_dynamodb_table.recipe_sharing_table.arn
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "dynamodb_access_attachment" {
-  role       = aws_iam_role.recipe_sharing_app_ec2_role.name
-  policy_arn = aws_iam_policy.dynamodb_access.arn
-}
-
-resource "aws_iam_instance_profile" "recipe_sharing_app_ec2_profile" {
-  name = "recipe_sharing_app_ec2_profile"
-  role = aws_iam_role.recipe_sharing_app_ec2_role.name
-}
-
-resource "aws_launch_template" "recipe_sharing_app_template" {
-  name_prefix   = "recipe_sharing_app_template"
-  image_id      = "ami-05ffe3c48a9991133"
-  instance_type = "t2.micro"
-
-  iam_instance_profile {
-    name = aws_iam_instance_profile.recipe_sharing_app_ec2_profile.name
-  }
-
-  network_interfaces {
-    associate_public_ip_address = true
-    security_groups             = [aws_security_group.recipe_sharing_app_asg.id]
-  }
-}
-
-resource "aws_autoscaling_group" "recipe_sharing_app" {
-  name                = "recipe_sharing_app"
-  desired_capacity    = 1
-  max_size            = 1
-  min_size            = 1
-  vpc_zone_identifier = data.aws_subnets.default.ids
-
-  launch_template {
-    id      = aws_launch_template.recipe_sharing_app_template.id
-    version = "$Latest"
-  }
-
-  target_group_arns = [aws_lb_target_group.recipe_sharing_app.arn]
-}
-
-resource "aws_dynamodb_table" "recipe_sharing_table" {
-  name         = "recipe_sharing_table"
+resource "aws_dynamodb_table" "recipes" {
+  name         = "recipes"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
 
@@ -184,17 +30,320 @@ resource "aws_dynamodb_table" "recipe_sharing_table" {
   }
 }
 
-resource "aws_s3_bucket" "recipe_sharing_site" {
-  bucket = "recipe-sharing-site"
+resource "aws_iam_policy" "recipe_ec2_instance_policy" {
+  name        = "ec2-instance-policy"
+  description = "Policy for EC2 instance to access DynamoDB"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:Scan",
+          "dynamodb:DeleteItem"
+        ]
+        Resource = aws_dynamodb_table.recipes.arn
+      }
+    ]
+  })
 }
 
-resource "aws_s3_bucket_acl" "recipe_sharing_site_acl" {
-  bucket = aws_s3_bucket.recipe_sharing_site.id
-  acl    = "private"
+resource "aws_iam_role" "recipe_ec2_instance_role" {
+  name = "ec2-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-resource "aws_s3_bucket_public_access_block" "recipe_sharing_site_public_access" {
-  bucket = aws_s3_bucket.recipe_sharing_site.id
+resource "aws_iam_role_policy_attachment" "ec2_policy_attach" {
+  role       = aws_iam_role.recipe_ec2_instance_role.name
+  policy_arn = aws_iam_policy.recipe_ec2_instance_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_policy_attach" {
+  role       = aws_iam_role.recipe_ec2_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.recipe_ec2_instance_role.name
+}
+
+resource "aws_vpc" "recipe_main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  instance_tenancy     = "default"
+
+  tags = {
+    Name = "Chapter 3 VPC"
+  }
+}
+
+resource "aws_internet_gateway" "recipe_main" {
+  vpc_id = aws_vpc.recipe_main.id
+
+  tags = {
+    Name = "Chapter 3 IGW"
+  }
+}
+
+resource "aws_subnet" "recipe_private_1" {
+  vpc_id            = aws_vpc.recipe_main.id
+  availability_zone = data.aws_availability_zones.available.names[0]
+  cidr_block        = "10.0.2.0/24"
+
+  tags = {
+    Name = "Private Subnet 1"
+  }
+}
+
+resource "aws_subnet" "recipe_private_2" {
+  vpc_id            = aws_vpc.recipe_main.id
+  availability_zone = data.aws_availability_zones.available.names[1]
+  cidr_block        = "10.0.3.0/24"
+
+  tags = {
+    Name = "Private Subnet 2"
+  }
+}
+
+resource "aws_route_table" "recipe_private" {
+  vpc_id = aws_vpc.recipe_main.id
+
+  tags = {
+    Name = "Private Route Table"
+  }
+}
+
+resource "aws_route_table_association" "recipe_private_1" {
+  subnet_id      = aws_subnet.recipe_private_1.id
+  route_table_id = aws_route_table.recipe_private.id
+}
+
+resource "aws_route_table_association" "recipe_private_2" {
+  subnet_id      = aws_subnet.recipe_private_2.id
+  route_table_id = aws_route_table.recipe_private.id
+}
+
+resource "aws_subnet" "recipe_public_1" {
+  vpc_id                  = aws_vpc.recipe_main.id
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet 1"
+  }
+}
+
+resource "aws_subnet" "recipe_public_2" {
+  vpc_id                  = aws_vpc.recipe_main.id
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public Subnet 2"
+  }
+}
+
+resource "aws_route_table" "recipe_public" {
+  vpc_id = aws_vpc.recipe_main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.recipe_main.id
+  }
+
+  tags = {
+    Name = "Public Route Table"
+  }
+}
+
+resource "aws_route_table_association" "recipe_public_1" {
+  subnet_id      = aws_subnet.recipe_public_1.id
+  route_table_id = aws_route_table.recipe_public.id
+}
+
+resource "aws_route_table_association" "recipe_public_2" {
+  subnet_id      = aws_subnet.recipe_public_2.id
+  route_table_id = aws_route_table.recipe_public.id
+}
+
+resource "aws_eip" "recipe_nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "recipe_main" {
+  allocation_id = aws_eip.recipe_nat.id
+  subnet_id     = aws_subnet.recipe_public_1.id
+
+  tags = {
+    Name = "Chapter 3 NAT Gateway"
+  }
+}
+
+resource "aws_route" "recipe_private_nat" {
+  route_table_id         = aws_route_table.recipe_private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.recipe_main.id
+}
+
+resource "aws_security_group" "recipe_alb" {
+  name        = "alb-sg"
+  description = "Allow HTTPS"
+  vpc_id      = aws_vpc.recipe_main.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2" {
+  name        = "ec2-sg"
+  description = "Allow HTTP from ALB"
+  vpc_id      = aws_vpc.recipe_main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.recipe_alb.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "recipe_main" {
+  ami                    = data.aws_ssm_parameter.recipe_latest_ami_id.value
+  instance_type          = var.instance_type
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
+  subnet_id              = aws_subnet.recipe_private_1.id
+  vpc_security_group_ids = [aws_security_group.ec2.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt update
+    sudo apt install -y python3 python3-pip python3-virtualenv nginx jq
+    git clone ${var.git_repo_url}
+    cp -r $(echo "${var.git_repo_url}" | sed 's/.*\///' | sed 's/\.git//')/recipe_sharing_app/backend . ; rm -rf $(echo "${var.git_repo_url}" | sed 's/.*\///' | sed 's/\.git//') ; cd backend
+
+    sed -i "s/SELECTED_REGION/$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')/g" main.py
+    
+    # Create an Nginx configuration file
+    cat << EOL > /etc/nginx/sites-available/fastapi
+    server {
+    listen 80;
+    server_name ~.;
+    location / {
+    proxy_pass http://localhost:8000;
+    }
+    }
+    EOL
+    
+    sudo ln -s /etc/nginx/sites-available/fastapi /etc/nginx/sites-enabled/
+    sudo systemctl restart nginx
+    virtualenv .venv
+    source .venv/bin/activate
+    pip install -r requirements.txt
+    python3 -m uvicorn main:app &
+  EOF
+
+  tags = {
+    Name = "Chapter 3 Instance"
+  }
+}
+
+resource "aws_lb" "recipe_main" {
+  name               = "chapter-3-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.recipe_alb.id]
+  subnets            = [aws_subnet.recipe_public_1.id, aws_subnet.recipe_public_2.id]
+}
+
+resource "aws_lb_target_group" "recipe_main" {
+  name        = "chapter-3-tg"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.recipe_main.id
+  target_type = "instance"
+
+  health_check {
+    path = "/health"
+  }
+}
+
+resource "aws_lb_listener" "recipe_https" {
+  load_balancer_arn = aws_lb.recipe_main.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  certificate_arn   = var.alb_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.recipe_main.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "recipe_main" {
+  target_group_arn = aws_lb_target_group.recipe_main.arn
+  target_id        = aws_instance.recipe_main.id
+  port             = 80
+}
+
+resource "aws_s3_bucket" "recipe_sharing_app" {
+  bucket = "frontend-chapter-3-${random_id.stack.hex}"
+
+  tags = {
+    Name = "Frontend Bucket"
+  }
+}
+
+resource "random_id" "stack" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket_website_configuration" "recipe_sharing_app" {
+  bucket = aws_s3_bucket.recipe_sharing_app.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "recipe_sharing_app" {
+  bucket = aws_s3_bucket.recipe_sharing_app.id
 
   block_public_acls       = true
   block_public_policy     = true
@@ -202,52 +351,48 @@ resource "aws_s3_bucket_public_access_block" "recipe_sharing_site_public_access"
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_versioning" "recipe_sharing_site_versioning" {
-  bucket = aws_s3_bucket.recipe_sharing_site.id
-  versioning_configuration {
-    status = "Disabled"
-  }
+resource "aws_cloudfront_origin_access_identity" "recipe_sharing_app" {
+  comment = "Origin Access Identity for ${aws_s3_bucket.recipe_sharing_app.id}"
 }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "recipe_sharing_site_encryption" {
-  bucket = aws_s3_bucket.recipe_sharing_site.id
+resource "aws_s3_bucket_policy" "recipe_sharing_app" {
+  bucket = aws_s3_bucket.recipe_sharing_app.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm     = "AES256"
-      kms_master_key_id = null
-    }
-    bucket_key_enabled = true
-  }
-}
-
-
-
-resource "aws_s3_bucket_website_configuration" "recipe_sharing_site_website" {
-  bucket = aws_s3_bucket.recipe_sharing_site.id
-
-  index_document {
-    suffix = "index.html"
-  }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.recipe_sharing_app.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.recipe_sharing_app.arn}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_cloudfront_distribution" "recipe_sharing_app" {
   origin {
-    domain_name = aws_s3_bucket.recipe_sharing_site.bucket_regional_domain_name
-    origin_id   = "S3-recipe-sharing-site"
+    domain_name = aws_s3_bucket.recipe_sharing_app.bucket_regional_domain_name
+    origin_id   = "origin-s3-${aws_s3_bucket.recipe_sharing_app.id}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.recipe_sharing_app.cloudfront_access_identity_path
+    }
   }
 
   enabled             = true
   is_ipv6_enabled     = true
-  comment             = "recipe_sharing_app"
   default_root_object = "index.html"
-
-  aliases = ["recipe.monvillarin.com"]
+  http_version        = "http2"
+  price_class         = "PriceClass_100"
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-recipe-sharing-site"
+    target_origin_id = "origin-s3-${aws_s3_bucket.recipe_sharing_app.id}"
 
     forwarded_values {
       query_string = false
@@ -262,27 +407,47 @@ resource "aws_cloudfront_distribution" "recipe_sharing_app" {
     max_ttl                = 86400
   }
 
-  price_class = "PriceClass_100"
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
+  custom_error_response {
+    error_code            = 403
+    response_code         = 200
+    response_page_path    = "/index.html"
+    error_caching_min_ttl = 0
   }
 
   viewer_certificate {
     acm_certificate_arn = "arn:aws:acm:us-east-1:026045577315:certificate/6f9106a0-d143-4bdb-8d9c-60ec70b4e3ee"
     ssl_support_method  = "sni-only"
   }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
 }
 
-data "aws_route53_zone" "monvillarin" {
+output "cloudfront_distribution_url" {
+  description = "URL of the CloudFront distribution to Access our frontend"
+  value       = aws_cloudfront_distribution.recipe_sharing_app.domain_name
+}
+
+output "cloudfront_distribution_id" {
+  description = "The CloudFront Distribution ID"
+  value       = aws_cloudfront_distribution.recipe_sharing_app.id
+}
+
+output "application_load_balancer_dns_name" {
+  description = "The DNS name of the Application Load Balancer"
+  value       = aws_lb.recipe_main.dns_name
+}
+
+data "aws_route53_zone" "main" {
   name = "monvillarin.com"
 }
 
-resource "aws_route53_record" "recipe_monvillarin_com" {
-  zone_id = data.aws_route53_zone.monvillarin.zone_id
-  name    = "recipe.${data.aws_route53_zone.monvillarin.name}"
+resource "aws_route53_record" "recipe" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "recipe.${data.aws_route53_zone.main.name}"
   type    = "A"
 
   alias {
@@ -292,14 +457,14 @@ resource "aws_route53_record" "recipe_monvillarin_com" {
   }
 }
 
-resource "aws_route53_record" "recipe_monvillarin_com" {
-  zone_id = data.aws_route53_zone.monvillarin.zone_id
-  name    = "api.${data.aws_route53_zone.monvillarin.name}"
+resource "aws_route53_record" "api" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = "api.${data.aws_route53_zone.main.name}"
   type    = "A"
 
   alias {
-    name                   = aws_lb.recipe_sharing_app.dns_name
-    zone_id                = aws_lb.recipe_sharing_app.zone_id
+    name                   = aws_lb.recipe_main.dns_name
+    zone_id                = aws_lb.recipe_main.zone_id
     evaluate_target_health = true
   }
 }
---
+++ Iac/variables.tf
@@ -0,0 +1,17 @@
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "alb_certificate_arn" {
+  description = "The ARN of the SSL certificate for the Application Load Balancer"
+  type        = string
+  default     = "arn:aws:acm:us-east-1:026045577315:certificate/6f9106a0-d143-4bdb-8d9c-60ec70b4e3ee"
+}
+
+variable "git_repo_url" {
+  description = "URL of the Git repository to clone"
+  type        = string
+  default     = "https://github.com/kurokood/recipe_sharing_app.git"
+}
